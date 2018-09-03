82d4d7dd474dbc5b3ac1ac378a4d6e7b6f1f1303
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-09-03 06:31:53 -0300

    Use Fragmetn

--- packages/ui/src/modules/common/components/SegmentHeader/index.tsx
+++ packages/ui/src/modules/common/components/SegmentHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Header, Icon, Divider, SemanticICONS } from 'semantic-ui-react';
 
 export interface ISegmentHeaderProps {
@@ -9,11 +9,11 @@ export const SegmentHeader: React.SFC<ISegmentHeaderProps> = ({
   title,
   icon,
 }) => (
-  <>
+  <Fragment>
     <div style={{display: 'flex', alignItems: 'center'}}>
       <Icon name={icon} size='small' />
       <Header as='h5' style={{margin: 0}}>{title}</Header>
     </div>
     <Divider style={{marginLeft: -14, marginRight: -14}} />
-  </>
+  </Fragment>
 )
