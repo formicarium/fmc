61ad9a91e0efdf1563e3c6dbed3103bd9e19a2dc
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-09-10 04:12:31 -0300

    Fix system import

--- packages/ui/src/modules/common/components/WithFMCSystem/index.tsx
+++ packages/ui/src/modules/common/components/WithFMCSystem/index.tsx
@@ -1,4 +1,4 @@
-import { WithSystem } from '~modules/core/components/SystemProvider';
+import { WithSystem } from '~/modules/core/components/SystemProvider';
 import { ISystem } from '~/system';
 
 export class WithFMCSystem extends WithSystem<ISystem> {}
