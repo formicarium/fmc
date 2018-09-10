e4c789d1976823874f621fa3c1dcfee321022a1f
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-09-09 23:58:41 -0300

    Use spanFilter instead of explorerState

--- packages/ui/src/modules/tracing/selectors/messages.ts
+++ packages/ui/src/modules/tracing/selectors/messages.ts
@@ -1,12 +1,11 @@
 import { getGraphFromEvents } from '~/modules/tracing/logic/event-graph';
-import { IExplorerState } from './../state/ExplorerState/index';
 import { IEventMessage } from '~/modules/tracing/model/event';
 import memoizeOne from 'memoize-one';
 import R from 'ramda'
 
-export const getFilteredMessages = memoizeOne((messages: IEventMessage[], explorerState: IExplorerState) =>
-  messages.filter((message) =>
-    new RegExp(`${explorerState.spanFilter}(\\.(.*))?$`).test(message.meta.spanId)))
+export const getFilteredMessages = memoizeOne((messages: IEventMessage[], spanFilter: string) => {
+  return messages.filter((message) => new RegExp(`${spanFilter}(\\.(.*))?$`).test(message.meta.spanId))
+})
 
 const getTimestamp = (message: IEventMessage) => message.meta.timestamp
 export const getSortedMessages = memoizeOne((messages: IEventMessage[]) => R.sortBy(getTimestamp, messages))
