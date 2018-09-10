341629f05f09e7d40da0e6eb1295dd1c464b35a1
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-09-09 23:44:42 -0300

    getSortedMessages selecto

--- packages/ui/src/modules/tracing/selectors/messages.ts
+++ packages/ui/src/modules/tracing/selectors/messages.ts
@@ -1,7 +1,11 @@
 import { IExplorerState } from './../state/ExplorerState/index';
 import { IEventMessage } from '~/modules/tracing/model/event';
 import memoizeOne from 'memoize-one';
+import R from 'ramda'
 
 export const getFilteredMessages = memoizeOne((messages: IEventMessage[], explorerState: IExplorerState) =>
   messages.filter((message) =>
     new RegExp(`${explorerState.spanFilter}(\\.(.*))?$`).test(message.meta.spanId)))
+
+const getTimestamp = (message: IEventMessage) => message.meta.timestamp
+export const getSortedMessages = memoizeOne((messages: IEventMessage[]) => R.sortBy(getTimestamp, messages))
