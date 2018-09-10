61d3f3b16e05adb006cad425392b767b166e760d
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-09-10 02:53:47 -0300

    expandedSectionsState

--- packages/ui/src/modules/tracing/state/DashboardState/index.ts
+++ packages/ui/src/modules/tracing/state/DashboardState/index.ts
@@ -1,14 +1,29 @@
 import { Container } from 'unstated';
 interface IDashboardState {
   showFilter: boolean
+  expandedSections: {
+    filter: boolean,
+    spanExplorer: boolean,
+    eventList: boolean,
+  }
 }
 
 export class DashboardState extends Container<IDashboardState> {
   public state = {
     showFilter: false,
-
+    expandedSections: {
+      filter: false,
+      spanExplorer: false,
+      eventList: false,
+    },
   }
   public toggleShowFilter = () => this.setState({
     showFilter: !this.state.showFilter,
   })
+  public setExpandedSection = (expandedSectionId: string, open: boolean) => this.setState((state) => ({
+    expandedSections: {
+      ...state.expandedSections,
+      [expandedSectionId]: open,
+    },
+  }))
 }
