359d682d43c2b41a329a8454d2007306dae1a8e4
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-09-10 04:13:57 -0300

    Using common CollapsableItem

--- packages/ui/src/modules/tracing/components/FilterPalette/index.tsx
+++ packages/ui/src/modules/tracing/components/FilterPalette/index.tsx
@@ -1,43 +1,15 @@
 import React from 'react'
-import { Header, Segment, Icon, SemanticICONS } from 'semantic-ui-react';
 import { SpanExplorerContainer } from '~/modules/tracing/containers/SpanExplorer';
 import { FilterContainer } from '~/modules/tracing/containers/Filter';
 import { EventListContainer } from '~/modules/tracing/containers/EventList';
 import { Subscribe } from 'unstated';
 import { DashboardState } from '~/modules/tracing/state/DashboardState';
+import { CollapsableItem } from '~/modules/common/components/CollapsableItem';
 
 export interface IFilterPaletteProps {
   activeIndex: number
 }
 
-const CollapsableItem: React.SFC<{
-  id: string,
-  text: string,
-  iconName: SemanticICONS,
-  open: boolean,
-  onToggle: (id: string, open: boolean) => void,
-}> = ({
-  id,
-  text,
-  open,
-  iconName,
-  children,
-  onToggle,
-}) => (
-  <div>
-    <Segment secondary padded={true} style={{margin: 0, display: 'flex', flexDirection: 'row', alignItems: 'center', cursor: 'pointer'}} onClick={() => onToggle(id, !open)}>
-      <div style={{flexGrow: 1, display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-        <Icon name={iconName} />
-        <Header as='h2' style={{margin: 0, marginLeft: 10}}>{text}</Header>
-      </div>
-
-      <Icon name={open ? 'chevron up' : 'chevron down'} />
-    </Segment>
-
-    {open ? children : null}
-  </div>
-
-)
 export const FilterPalette: React.SFC<IFilterPaletteProps> = ({
   activeIndex,
 }) => (
