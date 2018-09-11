98fb285597f89630a9f38cee96ffbe03ad4c657d
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-09-11 09:14:08 -0300

    Added REACT and APOLLO dev tools

--- packages/ui/electron/main.js
+++ packages/ui/electron/main.js
@@ -3,16 +3,20 @@ const electron = require('electron');
 const {app, BrowserWindow} = electron;
 const path = require('path');
 const url = require('url');
+const {default: installExtension, REACT_DEVELOPER_TOOLS, APOLLO_DEVELOPER_TOOLS } = require('electron-devtools-installer');
 
-// Let electron reloads by itself when webpack watches changes in ./app/
-// if (process.env.ELECTRON_START_URL) {
-//   require('electron-reload')(__dirname);
-// }
+const installAndLog = (extension) => installExtension(extension)
+    .then((name) => console.log(`Added Extension:  ${name}`))
+    .catch((err) => console.log('An error occurred: ', err))
+
 
 // To avoid being garbage collected
 let mainWindow;
 
 app.on('ready', () => {
+  installAndLog(REACT_DEVELOPER_TOOLS)
+  .then(() => installAndLog(APOLLO_DEVELOPER_TOOLS))
+
   mainWindow = new BrowserWindow({
     width: 800, height: 600,
     webPreferences: {
