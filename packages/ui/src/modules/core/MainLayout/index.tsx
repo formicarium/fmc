a55a9f88a2f52d8a1430613a6dacb6d84fbd4a0a
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-09-11 04:48:37 -0300

    Removed key handlers for menu on MainLayout

--- packages/ui/src/modules/core/MainLayout/index.tsx
+++ packages/ui/src/modules/core/MainLayout/index.tsx
@@ -5,7 +5,7 @@ import { Routes } from '../components/Routes';
 import { ToastContainer } from 'react-toastify';
 import { SyncState } from '~/modules/sync/state/SyncState';
 import styled from 'styled-components';
-import {HotKeys} from 'react-hotkeys';
+// import {HotKeys} from 'react-hotkeys';
 import { withRouter, RouteComponentProps } from 'react-router';
 import { Subscribe } from 'unstated';
 import { DashboardState } from '~/modules/tracing/state/DashboardState';
@@ -13,14 +13,19 @@ import { DashboardState } from '~/modules/tracing/state/DashboardState';
 interface IMainLayoutProps {
   syncState: SyncState
 }
-// const MenuWrapper = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-// `
+
 const RoutesWrapper = styled.div`
   padding-top: 14px;
+  align-self: stretch;
 `
+
+const MainLayoutWrapper = styled(Container)`
+  padding-top: 14px;
+  display: flex !important;
+  flex-direction: column;
+  align-items: center;
+`
+
 export type IProps = IMainLayoutProps & RouteComponentProps<{}>
 export class MainLayoutInner extends React.Component<IProps> {
   constructor(props: IProps) {
@@ -32,47 +37,46 @@ export class MainLayoutInner extends React.Component<IProps> {
       });
     }
   }
-  private map = {
-    selectFirstItem: 'command+1',
-    selectSecondItem: 'command+2',
-    selectThirdItem: 'command+3',
-    selectFourthItem: 'command+4',
-    selectFifthItem: 'command+5',
-  };
+  // private map = {
+  //   selectFirstItem: 'command+1',
+  //   selectSecondItem: 'command+2',
+  //   selectThirdItem: 'command+3',
+  //   selectFourthItem: 'command+4',
+  //   selectFifthItem: 'command+5',
+  // };
 
   private navigateToItem = (item: string) => () => {
     this.props.history.push(item)
   }
 
-  private handlers = {
-    selectFirstItem: this.navigateToItem('/'),
-    selectSecondItem: this.navigateToItem('/devspaces/create'),
-    selectThirdItem: this.navigateToItem('/my-devspace'),
-    selectFourthItem: this.navigateToItem('/tracing'),
-    selectFifthItem: this.navigateToItem('/sync'),
-  }
+  // private handlers = {
+  //   selectFirstItem: this.navigateToItem('/'),
+  //   selectSecondItem: this.navigateToItem('/devspaces/create'),
+  //   selectThirdItem: this.navigateToItem('/my-devspace'),
+  //   selectFourthItem: this.navigateToItem('/tracing'),
+  //   selectFifthItem: this.navigateToItem('/sync'),
+  // }
 
   public render() {
     return (
-      <HotKeys keyMap={this.map}>
-        <HotKeys handlers={this.handlers}>
-          <Container style={{paddingTop: 14}}>
-            <ToastContainer />
-              <Subscribe to={[DashboardState]}>
-                {(dashboard: DashboardState) => (
-                  <Transition visible={!dashboard.state.showFilter} animation='fade down' duration={300}>
-                    <div>
-                      <TopMenu />
-                    </div>
-                  </Transition>
-                )}
-              </Subscribe>
-            <RoutesWrapper>
-              <Routes />
-            </RoutesWrapper>
-          </Container>
-        </HotKeys>
-      </HotKeys>
+      <MainLayoutWrapper>
+        <ToastContainer />
+        <Subscribe to={[DashboardState]}>
+          {(dashboard: DashboardState) => {
+            return (
+              <Transition visible={!dashboard.state.showFilter} animation='fade down' duration={300}>
+                {/* This div is here because without it, Transition does not work for some reason */}
+                <div>
+                  <TopMenu />
+                </div>
+              </Transition>
+            )
+          }}
+        </Subscribe>
+        <RoutesWrapper>
+          <Routes />
+        </RoutesWrapper>
+      </MainLayoutWrapper>
     )
   }
 }
