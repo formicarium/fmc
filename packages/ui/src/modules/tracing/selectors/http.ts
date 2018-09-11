df991b1174cca483dc9c0dbd7a1e405aa36c63e1
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2018-09-11 04:07:45 -0300

    Naive algorithm to draw http panel request/response

--- packages/ui/src/modules/tracing/selectors/http.ts
+++ packages/ui/src/modules/tracing/selectors/http.ts
@@ -2,6 +2,7 @@ import { createSelector } from 'reselect';
 import { IDashboardState } from '~/modules/tracing/state/DashboardState';
 import { IEventMessage } from '~/modules/tracing/model/event';
 import { httpEventToRequest, httpEventToResponse } from '~/modules/tracing/logic/event-http';
+import { isRequest } from '~/modules/tracing/components/HTTP/logic';
 
 export interface IStateGroup {
   dashboardState: IDashboardState
@@ -20,21 +21,34 @@ export const getHttpPanelRequestsAndResponses = createSelector(
   selectedEdgeSelector,
   (events, selectedEdge) => {
     if (!selectedEdge) { return null }
-    console.log('selectedEdge')
-    console.log(selectedEdge)
     const sourceHttpEvent = events.find((event) => event.id === selectedEdge.metadata.fromEvent)
     const destHttpEvent = events.find((event) => event.id === selectedEdge.metadata.toEvent)
-    console.log('sourceHttpEvent')
-    console.log(sourceHttpEvent)
-    console.log('destHttpEvent')
-    console.log(destHttpEvent)
-    const outProducer = httpEventToRequest(sourceHttpEvent, destHttpEvent.meta.service)
-    const inConsumer = httpEventToRequest(destHttpEvent, sourceHttpEvent.meta.service)
-    return {
-      outProducer,
-      inConsumer,
-      inProducer: outProducer,
-      outConsumer: inConsumer,
+    const sourceHttpEventPair = events.find((event) => event.meta.spanId === sourceHttpEvent.meta.spanId && event.id !== sourceHttpEvent.id)
+    const destHttpEventPair = events.find((event) => event.meta.spanId === destHttpEvent.meta.spanId && event.id !== destHttpEvent.id)
+
+    if (isRequest(sourceHttpEvent.payload.type, sourceHttpEvent.payload.direction)) {
+      const outProducer = httpEventToRequest(sourceHttpEvent, destHttpEvent.meta.service)
+      const inConsumer = httpEventToRequest(destHttpEvent, sourceHttpEvent.meta.service)
+      const outConsumer = httpEventToRequest(sourceHttpEventPair, destHttpEventPair.meta.service)
+      const inProducer = httpEventToRequest(destHttpEventPair, sourceHttpEventPair.meta.service)
+      return {
+        outProducer,
+        inConsumer,
+        inProducer,
+        outConsumer,
+      }
+    } else {
+      const outProducer = httpEventToRequest(destHttpEventPair, sourceHttpEventPair.meta.service)
+      const inConsumer = httpEventToRequest(sourceHttpEventPair, destHttpEventPair.meta.service)
+      const outConsumer = httpEventToRequest(destHttpEvent, sourceHttpEvent.meta.service)
+      const inProducer = httpEventToRequest(sourceHttpEvent, destHttpEvent.meta.service)
+      return {
+        outProducer,
+        inConsumer,
+        inProducer,
+        outConsumer,
+      }
     }
+
   }
 )
