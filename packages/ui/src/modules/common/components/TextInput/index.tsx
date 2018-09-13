ede1adc50876a1093fece54b4f7ad4447263265b
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-13 05:07:15 -0300

    Textinput final form component

---
+++ packages/ui/src/modules/common/components/TextInput/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FieldRenderProps } from 'react-final-form';
+import { Input } from 'semantic-ui-react'
+
+export const TextInput: React.SFC<FieldRenderProps> = ({
+  input,
+  meta,
+  ...rest
+}) => {
+  return (
+    <Input
+      {...rest}
+      error
+      value={input.value}
+      onChange={(_, { value }) => input.onChange(value)}
+    />
+  )
+}
