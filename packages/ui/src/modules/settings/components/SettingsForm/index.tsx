0dde037e2febdc8708bdb7a1f0b8d1e9f40ee2a9
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-09-13 05:07:26 -0300

    Validate kubectl bin

--- packages/ui/src/modules/settings/components/SettingsForm/index.tsx
+++ packages/ui/src/modules/settings/components/SettingsForm/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Form as SemanticForm, Button, Segment } from 'semantic-ui-react';
 import { Form, Field } from 'react-final-form'
+import { IKubectlVersion, IMinMajor } from '@formicarium/common'
+import compareVersions from 'compare-versions'
+import { TextInput } from '~/modules/common/components/TextInput';
+import _ from 'lodash'
 
 const validate = () => {
   return {}
@@ -13,25 +17,49 @@ export interface ISettingsFormValue {
 export interface ISettingsForm {
   initialValues: Partial<ISettingsFormValue>
   onSubmit: (values: ISettingsFormValue) => void
+  getVersionForKubectlBin: (kubectlBin: string) => Promise<IKubectlVersion>
 }
+
+const minMajorToSemanticString = (minMajor: IMinMajor) => `${minMajor.major}.${minMajor.minor}.0`
+const MIN_KUBECTL_VERSION: IMinMajor = {
+  major: '1',
+  minor: '9'
+}
+
+const validateKubectlBin = (getVersionForKubectlBin: ISettingsForm['getVersionForKubectlBin']) => _.memoize(async (value: string) => {
+  try {
+    const { clientVersion } = await getVersionForKubectlBin(value)
+    if (compareVersions(
+      minMajorToSemanticString(clientVersion),
+      minMajorToSemanticString(MIN_KUBECTL_VERSION)
+    ) === -1) {
+      const { major, minor } = MIN_KUBECTL_VERSION
+      return `Version needs to be greater than ${major}.${minor}`
+    }
+  } catch (err) {
+    return 'Invalid bin!'
+  }
+})
 
 export const SettingsForm: React.SFC<ISettingsForm> = ({
   onSubmit,
   initialValues,
+  getVersionForKubectlBin,
 }) => (
   <Segment>
     <Form
       onSubmit={onSubmit}
       initialValues={initialValues}
       validate={validate}
-      render={({handleSubmit, submitting, invalid}) => {
+      render={({handleSubmit, submitting, invalid, validating, errors }) => {
         return (
           <SemanticForm onSubmit={handleSubmit}>
-            <SemanticForm.Field disabled={submitting}>
-              <label>Kubectl binary</label>
+            <SemanticForm.Field disabled={submitting} error={errors.kubectlBin}>
+              <label>Kubectl bin</label>
               <Field
+                validate={validateKubectlBin(getVersionForKubectlBin)}
                 name='kubectlBin'
-                component='input'
+                component={TextInput}
                 placeholder='Name'
               />
             </SemanticForm.Field>
@@ -39,7 +67,7 @@ export const SettingsForm: React.SFC<ISettingsForm> = ({
               <Button
                 type='submit'
                 color='purple'
-                loading={submitting}
+                loading={submitting || validating}
                 disabled={submitting || invalid}
                 basic >
                 Save
