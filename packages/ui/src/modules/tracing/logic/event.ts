2c4ba861b7bb1fbb52b412488809329f2bc5d842
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2018-10-12 02:40:17 -0300

    Event logic

---
+++ packages/ui/src/modules/tracing/logic/event.ts
@@ -0,0 +1,9 @@
+import R from 'ramda'
+import { IEvent, SpanDirection, SpanType } from '~/modules/tracing/graphql/queries/events';
+
+export const getParentIdFromEvent = (event: IEvent): string => R.pathOr('', ['payload', 'context', 'parentId'], event)
+export const getSpanIdFromEvent = (event: IEvent): string => R.pathOr('', ['payload', 'context', 'spanId'], event)
+export const getDirectionFromEvent = (event: IEvent): SpanDirection => R.pathOr('', ['payload', 'tags', 'direction'], event)
+export const getTypeFromEvent = (event: IEvent): SpanType => R.pathOr('', ['payload', 'tags', 'type'], event)
+export const getReporterId = (event: IEvent) => event.meta.service
+export const getTimestampFromEvent = (event: IEvent) => R.pathOr(null, ['meta', 'timestamp'])
