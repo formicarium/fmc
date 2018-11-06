f0c599bb01acef6e2da621164ab115811c99e65f
Leonardo Iacovini <leoiacovini@gmail.com>
2018-11-06 14:33:47 -0200

    Add fmc curl command and stop eating errors

---
+++ packages/cli/src/commands/curl.ts
@@ -0,0 +1,73 @@
+import FMCCommand from '../FMCCommand'
+import { flags as Flags } from '@oclif/command'
+import { spawn } from 'child_process'
+import * as inquirer from 'inquirer'
+
+export default class Curl extends FMCCommand {
+  public static description = 'Make a curl request to the service in the devspace being used. You can pass any extra arguments to curl at the end of the command'
+
+  public static strict = false
+  public static examples = [
+    `$ fmc curl GET purgatory /api/version`,
+    `$ fmc curl POST -i default purgatory /do/something`,
+    `$ fmc curl POST purgatory /do/something -d '{...}'`,
+  ]
+
+  public static flags = {
+    ...FMCCommand.flags,
+    help: Flags.help({ char: 'h' }),
+    interface: Flags.string({
+      char: 'i',
+      helpValue: '',
+      name: 'interface',
+      description: 'Interface to send the request',
+    }),
+  }
+
+  public static args = [
+    {
+      name: 'method',
+      required: true,
+    },
+    {
+      name: 'serviceName',
+      required: true,
+    },
+    {
+      name: 'path',
+      required: true,
+    },
+  ]
+
+  private request(method: string, url: string, opts: any) {
+    spawn('curl', [`-X${method}`, url, ...opts], {
+      stdio: [process.stdin, process.stdout, process.stderr],
+    })
+  }
+
+  private async getLink(links: any) {
+    const { flags } = this.parse(Curl)
+    if (flags.interface === undefined) {
+      const responses = await inquirer.prompt<{ interface: string }>([{
+        name: 'interface',
+        message: 'Select a Interface',
+        type: 'list',
+        choices: Object.keys(links),
+      }])
+      return links[responses.interface]
+    } else {
+      return links[flags.interface]
+    }
+  }
+
+  public async run() {
+    const { configService, soilService } = this.system
+    const { devspace: { name: currentDevspace } } = await configService.readConfig()
+    const { args: { serviceName, method, path }, argv } = this.parse(Curl)
+    const opts = argv.slice(3)
+    // console.log(opts)
+    const { links } = await soilService.getService(currentDevspace, serviceName)
+    const url = `${await this.getLink(links)}${path}`
+    this.request(method, url, opts)
+  }
+}
