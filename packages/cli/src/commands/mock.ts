be66a3e2b0f30b45ae9cc304d6a2fd718caafafc
Rafael Leal <rafaelealdias@gmail.com>
2020-07-10 19:22:26 -0300

    Better output

--- packages/cli/src/commands/mock.ts
+++ packages/cli/src/commands/mock.ts
@@ -6,6 +6,7 @@ import { spawn } from 'child_process'
 import * as inquirer from 'inquirer'
 import { getFileContent } from '../common'
 import * as fs from 'fs-extra'
+import { UIService } from '../services/ui'
 
 
 export default class Mock extends FMCCommand {
@@ -63,7 +64,7 @@ export default class Mock extends FMCCommand {
   }
 
   public async run() {
-    const { pitfallService } = this.system
+    const { pitfallService, uiService, outputService } = this.system
     const currentDevspace = await this.currentDevspace()
 
     // const { devspace: { hiveReplUrl } } = await configService.readConfig()
@@ -72,7 +73,11 @@ export default class Mock extends FMCCommand {
     const { file, directory } = flags
 
     if (!file && !directory) {
-      this.error("You must provide `--file` or `--directory` flags")
+      // this.error("You must provide `--file` or `--directory` flags")
+
+      const mocks = await pitfallService.getMocks(currentDevspace)
+      console.log(JSON.stringify(mocks, null, 2))
+      return null
     }
     if (file && directory) {
       this.error("You can't define both `--file` and `--directory` flags")
@@ -80,9 +85,10 @@ export default class Mock extends FMCCommand {
 
     const mocks = await this.getMocks(file, directory)
     this.print(mocks)
-    console.log("upserted:")
     if (mocks) {
-      this.print(await pitfallService.upsertMocks(currentDevspace, mocks))
+      uiService.spinner.start('Updating mocks...')
+      await pitfallService.upsertMocks(currentDevspace, mocks)
+      uiService.spinner.succeed("Mocks updated!")
     } else {
       this.error("Error trying to resolve mocks configuration")
     }
